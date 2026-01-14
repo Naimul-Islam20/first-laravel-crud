26d0d09a55ab33634ad041b0bbe07b5f27e25e1f
Naimul Islam <naimulislam8828@gmail.com>
2026-01-14 19:37:14 +0600

    initial commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,17 @@
+<!doctype html>
+<html>
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <script src="https://cdn.tailwindcss.com"></script>
+</head>
+<body>
+  <div class="container mx-auto">
+    <div class="flex justify-between">
+
+      <h1 class="text-3xl font-bold">Home</h1>
+      <a href="/create" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Add to Post</a>
+    </div>
+  </div>
+</body>
+</html>
